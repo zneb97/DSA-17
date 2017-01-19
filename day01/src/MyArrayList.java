b8b653c755e50801831e4e2f0539599318550647
David Abrahams <davidabrahams@sbcglobal.net>
2017-01-18 19:35:50 -0500

    adding day01

---
+++ day01/src/MyArrayList.java
@@ -0,0 +1,30 @@
+public class MyArrayList {
+
+	public MyArrayList(){
+		// TODO
+	}
+
+	public void add(Cow c){
+		// TODO
+	}
+
+	public int size(){
+		// TODO
+		return -1;
+	}
+
+	public Cow get(int index){
+		// TODO
+		return null;
+	}
+
+	public Cow remove(int index){
+		// TODO
+		return null;
+	}
+
+	public void add(int index, Cow c){
+		// TODO
+	}
+
+}
