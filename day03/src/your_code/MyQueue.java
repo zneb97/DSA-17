c1c6f0e0561606804e045d9988f6ac2e345cba33
David Abrahams <davidabrahams@sbcglobal.net>
2017-01-31 00:32:40 -0500

    ship!

---
+++ day03/src/your_code/MyQueue.java
@@ -0,0 +1,32 @@
+package your_code;
+
+import ADTs.QueueADT;
+
+/**
+ * An implementation of the Queue interface.
+ */
+public class MyQueue implements QueueADT<Integer> {
+
+    @Override
+    public void enqueue(Integer item) {
+        // TODO
+    }
+
+    @Override
+    public Integer dequeue() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public Integer front() {
+        // TODO
+        return null;
+    }
+}
