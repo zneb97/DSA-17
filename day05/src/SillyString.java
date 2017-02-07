9b5ef37d9de22b2391c4072f2d3571b91a2e9aaa
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2017-02-07 11:43:35 -0500

    minor changes to day5

--- day05/src/SillyString.java
+++ day05/src/SillyString.java
@@ -19,7 +19,7 @@ public class SillyString {
 
 	@Override
 	public int hashCode() {
-		// TODO What is wrong with this hash function??
+		// TODO What is bad about this hash function??
 		int total = 0;
 		for (int i=0; i<innerString.length(); i++) {
 			total += innerString.charAt(i);
