23a6d05aba4160a03fd82b67a3edcc8a62a7886b
CleanestMink126 <steelman@students.olin.edu>
2017-02-12 23:58:28 -0500

    worked on index

--- search-engine/day00/src/Index.java
+++ search-engine/day00/src/Index.java
@@ -2,6 +2,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,16 +15,36 @@ public class Index {
 		// TODO
 		// if we're seeing a term for the first time, make a new Set
 		// otherwise we can add the term to an existing Set
+		if(index.containsKey(term)){
+			HashSet<TermCounter> insert =  (HashSet<TermCounter>)(index.get(term));
+			insert.add(tc);
+			index.put(term,insert);
+		}else{
+			HashSet<TermCounter> insert = new HashSet<TermCounter>();
+			insert.add(tc);
+			index.put(term, insert);
+		}
 	}
 
 	public Set<TermCounter> get(String term) {
-		// TODO
-		return null;
+		if(index.containsKey(term)){
+			return (HashSet<TermCounter>)(index.get(term));
+
+		}else{
+			throw new NullPointerException();
+		}
 	}
 
 	public void indexPage(String url, Elements paragraphs) {
 		// make a TermCounter and count the terms in the paragraphs
 		// TODO
+		TermCounter newCount = new TermCounter(url);
+		newCount.processElements(paragraphs);
+		Set<String> keySet = newCount.keySet();
+		for(String key : keySet){
+			this.add(key,newCount);
+		}
+
 
 		// for each term in the TermCounter, add the TermCounter to the index
 		// TODO
