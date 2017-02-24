4d812c97b416f05c5fae431a8658f0a1cf72ea93
David Abrahams <davidabrahams@sbcglobal.net>
2017-02-24 16:07:41 -0500

    making it testable

--- sorting/day01/src/HeapSort.java
+++ sorting/day01/src/HeapSort.java
@@ -1,6 +1,6 @@
 public class HeapSort extends SortAlgorithm {
-    private int size;
-    private int[] heap;
+    int size;
+    int[] heap;
 
     private int parent(int i) {
         return (i-1) / 2;
@@ -15,12 +15,12 @@ public class HeapSort extends SortAlgorithm {
     }
 
     // Recursively corrects the position of element indexed i: check children, and swap with larger child if necessary.
-    private void heapify(int i) {
+    public void heapify(int i) {
         // TODO
     }
 
     // Given the array, build a heap by correcting every non-leaf's position.
-    private void buildHeapFrom(int[] array) {
+    public void buildHeapFrom(int[] array) {
         this.heap = array;
         this.size = array.length;
         // TODO
