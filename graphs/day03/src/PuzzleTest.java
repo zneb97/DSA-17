b84940960dc936696632718280de9ebaf6f1bfd3
David Abrahams <davidabrahams@sbcglobal.net>
2017-04-05 01:07:34 -0400

    adding more tests for intermediate distances

--- graphs/day03/src/PuzzleTest.java
+++ graphs/day03/src/PuzzleTest.java
@@ -10,23 +10,23 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class PuzzleTest {
 
-	private Board board;
+    private Board board;
 
-	@Before
-	public void setUp() throws Exception {
-        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}};
+    @Before
+    public void setUp() throws Exception {
+        int[][] initState = {{1, 2, 3}, {4, 6, 0}, {7, 5, 8}};
         board = new Board(initState);
     }
 
     // Test board methods
 
-	/**
- 	* Test method for void manhattan().
- 	*/
-	@Test
-	public void testManhattan() {
-	    assertThat(board.manhattan(), is(1));
-	}
+    /**
+     * Test method for void manhattan().
+     */
+    @Test
+    public void testManhattan() {
+        assertThat(board.manhattan(), is(3));
+    }
 
     /**
      * Test method for boolean isGoal().
@@ -62,7 +62,7 @@ public class PuzzleTest {
         assertThat(solver.isSolvable(), is(true));
         // Create solution boards list
         int solns[][][] = {{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}},
-                            {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}}};
+                {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}}};
         //Compare to output
         int i = 0;
         for (Board board : solver.solution()) {
@@ -71,11 +71,8 @@ public class PuzzleTest {
         }
     }
 
-    /**
-     * Test method for Solver - Hard puzzle
-     */
     @Test
-    public void testSolverHard() {
+    public void testSolverMeh() {
 
         // Hard solve puzzle
         int[][] initState = {{0, 1, 3}, {4, 2, 5}, {7, 8, 6}};
@@ -86,10 +83,60 @@ public class PuzzleTest {
         assertThat(solver.minMoves, is(4));
     }
 
+
+    @Test
+    public void testSolverMedium() {
+
+        // Hard solve puzzle
+        int[][] initState = {{2, 3, 6}, {1, 5, 0}, {4, 7, 8}};
+        Board initial = new Board(initState);
+        Solver solver = new Solver(initial);
+        assertThat(solver.isSolvable(), is(true));
+        // Check number of moves
+        assertThat(solver.minMoves, is(7));
+    }
+
+    @Test
+    public void testSolverHard() {
+
+        // Hard solve puzzle
+        int[][] initState = {{0, 3, 5}, {2, 1, 8}, {4, 7, 6}};
+        Board initial = new Board(initState);
+        Solver solver = new Solver(initial);
+        assertThat(solver.isSolvable(), is(true));
+        // Check number of moves
+        assertThat(solver.minMoves, is(12));
+    }
+
+    @Test
+    public void testSolverReallyHard() {
+
+        // Hard solve puzzle
+        int[][] initState = {{3, 5, 6}, {1, 2, 8}, {0, 4, 7}};
+        Board initial = new Board(initState);
+        Solver solver = new Solver(initial);
+        assertThat(solver.isSolvable(), is(true));
+        // Check number of moves
+        assertThat(solver.minMoves, is(16));
+    }
+
+
+    @Test
+    public void testSolverSuperDuperHard() {
+
+        // Hard solve puzzle
+        int[][] initState = {{3, 5, 2}, {6, 0, 1}, {7, 8, 4}};
+        Board initial = new Board(initState);
+        Solver solver = new Solver(initial);
+        assertThat(solver.isSolvable(), is(true));
+        // Check number of moves
+        assertThat(solver.minMoves, is(22));
+    }
+
     /**
      * Test method for Solver - Hard puzzle
      */
-    @Test
+//    @Test
     public void testSolverInsane() {
 
         // Hard solve puzzle
