ab0fd094d5a25dd21b8655212bb721bebe306ec7
Sidd Singal <sidd@sidds-mbp.olin.edu>
2017-04-12 14:45:19 -0400

    added gold tests

--- DP/day00/test/SplitCoinsTest.java
+++ DP/day00/test/SplitCoinsTest.java
@@ -2,7 +2,12 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class SplitCoinsTest {
@@ -12,6 +17,28 @@ public class SplitCoinsTest {
      * Test method for {@link SplitCoins#splitCoins(int[])}.
      */
     @Test
-    public void testBillsNeeded() {
+    public void testSplitCoins() throws FileNotFoundException {
+
+        for(int i = 1; i <= 11; i++){
+
+            Scanner input = new Scanner(new File(
+                    "testcases/divgold." + i + ".in"));
+
+            Scanner output = new Scanner(new File(
+                    "testcases/divgold." + i + ".out"));
+
+            int n = input.nextInt();
+            int[] coins = new int[n];
+            for(int j = 0; j < n; j++){
+                coins[j] = input.nextInt();
+            }
+
+            assertEquals(output.nextInt(),new SplitCoins().splitCoins(coins));
+
+            input.close();
+            output.close();
+
+        }
+
     }
 }
