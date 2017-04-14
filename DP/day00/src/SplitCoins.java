f0545a857f3fbe40c5c15df622a00651465a93ee
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-04-13 21:58:22 -0400

    DP

--- DP/day00/src/SplitCoins.java
+++ DP/day00/src/SplitCoins.java
@@ -1,7 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class SplitCoins {
 
-    public static int splitCoins(int[] coins) {
-        // TODO
-        return -1;
+    public int splitCoins(int[] coins) {
+        int total = 0;
+        for (int i = 0; i < coins.length; i++) {
+            total += coins[i];
+        }
+
+        //Make memo
+        int[][] memo = new int[total+1][coins.length];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return recurse(total, 0 , 0, coins, memo);
+    }
+
+    private int recurse(int total, int used, int pile, int[] coins, int[][] memo){
+
+        //Base, used up all coins
+        if(coins.length == used){
+            return Math.abs(pile*2-total);
+        }
+
+        //Exists
+        else if(memo[pile][used] != -1){
+            return memo[pile][used];
+        }
+
+        //Continue
+        int left = recurse(total, used+1, pile + coins[used], coins, memo);
+        int right = recurse(total,used+1, pile, coins, memo);
+        int min = Math.min(left,right);
+        memo[pile][used] = min;
+        return min;
     }
 }
--- DP/day00/src/Stocks.java
+++ DP/day00/src/Stocks.java
@@ -1,13 +1,18 @@
 public class Stocks {
 
-    public int maxProfit(int[] prices) {
-        // TODO
-        return -1;
-    }
 
-    public int maxProfitWithK(int[] prices, int k) {
-        // TODO: Optional
-        return -1;
-    }
+    public static int maxProfit(int[] prices) {
+        //Bare Minimum
+        int difference = 0;
+        int profit = 0;
 
+        for (int i = 1; i < prices.length; ++i) {
+            difference += prices[i] - prices[i-1];
+            //Update
+            profit = Math.max(profit, difference);
+            difference = Math.max(0, difference);
+        }
+
+        return profit;
+    }
 }
